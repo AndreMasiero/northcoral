27de7d2de080b3db6fbb7db1865298a7ddeabb58
AndreMasiero <andreboavista@gmail.com>
2026-03-09 18:10:03 -0300

    first commit

---
+++ SW.js
@@ -0,0 +1,4 @@
+// Este é um Service Worker básico apenas para satisfazer os requisitos de instalação de um PWA.
+self.addEventListener('fetch', function(event) {
+    // Não faz nada complexo, apenas deixa as requisições passarem normalmente
+});
